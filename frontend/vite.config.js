2a0cb2f7e818884f7a95132f42a90c0d96eaf53c
Khushal <khushalr010@gmail.com>
2025-10-12 16:23:54 +0530

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // <-- make sure this is "/" for root deployment
-});
+  base: '/',
+})
