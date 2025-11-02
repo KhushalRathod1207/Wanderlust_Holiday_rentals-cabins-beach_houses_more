d31286a4d82ce91e09116cd9bc7539cb0dc26b1e
Khushal <khushalr010@gmail.com>
2025-11-02 12:13:38 +0530

    Deployee

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './assets/css/rating.css';
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./components/AuthContext.jsx";
 
 
 import App from './App.jsx'
