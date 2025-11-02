5c2a26d6da178a6069c5219e67adbce47327b5a6
Khushal <khushalr010@gmail.com>
2025-11-02 12:44:11 +0530

    Deployee

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Must be '/' for deployment
+  server: {
+    host: true,
+    port: 5173
+  },
+  build: {
+    outDir: "dist"
+  }
 });
